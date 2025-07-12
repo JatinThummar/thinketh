e36f215b65ebde784fa8e107241549b42b62fb7b
JatinThummar <thummarjatin1208@gmail.com>
2025-07-12 22:04:08 +0530

    Fix GitHub Pages paths and CSS

--- src/build.js
+++ src/build.js
@@ -3,16 +3,20 @@ const fs = require('fs-extra');
 const path = require('path');
 
 const SUPPORTED_LANGUAGES = ['en', 'hi'];
-const BUILD_DIR = path.join(__dirname, '../docs'); // GitHub Pages uses /docs or /root
+const BUILD_DIR = path.join(__dirname, '../docs');
+// Base path for GitHub Pages (empty string for local development)
+const BASE_PATH = '/thinketh';
 
 // Ensure build directory exists and is empty
 fs.emptyDirSync(BUILD_DIR);
 
-// Copy static assets
-fs.copySync(path.join(__dirname, '../build/css'), path.join(BUILD_DIR, 'css'));
-
-// Compile layout template
-const layoutTemplate = pug.compileFile(path.join(__dirname, 'includes/layout.pug'));
+// Copy CSS file
+const cssDir = path.join(BUILD_DIR, 'css');
+fs.ensureDirSync(cssDir);
+fs.copyFileSync(
+  path.join(__dirname, '../build/css/screen.css'),  // Original CSS path
+  path.join(cssDir, 'style.css')  // New CSS path
+);
 
 // Compile index for each language
 SUPPORTED_LANGUAGES.forEach(lang => {
@@ -24,6 +28,7 @@ SUPPORTED_LANGUAGES.forEach(lang => {
   const html = pug.renderFile(path.join(__dirname, 'index.pug'), {
     currentLocale: lang,
     supportedLanguages: SUPPORTED_LANGUAGES,
+    basePath: BASE_PATH,
     pretty: true
   });
 
@@ -38,11 +43,11 @@ const redirectHTML = `
   <head>
     <meta charset="utf-8">
     <title>Redirecting...</title>
-    <meta http-equiv="refresh" content="0; url=./en/">
-    <script>window.location.href = "./en/";</script>
+    <meta http-equiv="refresh" content="0; url=${BASE_PATH}/en/">
+    <script>window.location.href = "${BASE_PATH}/en/";</script>
   </head>
   <body>
-    <p>Redirecting to <a href="./en/">English version</a>...</p>
+    <p>Redirecting to <a href="${BASE_PATH}/en/">English version</a>...</p>
   </body>
 </html>
 `;
